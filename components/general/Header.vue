c5f5df312ed977b0c156ca93a1d3799c08b3ac2b
xacobe <xacobe@cambrico.net>
2020-11-12 15:15:55 +0100

    start menu-mobile

--- components/general/Header.vue
+++ components/general/Header.vue
@@ -1,18 +1,60 @@
 <template>
-  <nav class="mx-auto">
+  <header>
     <img src="../../assets/img/asociacion-monte-galego.jpg" alt="Asociación do monte galego" />
-    <ul class="flex 0 p-3">
-      <li class="flex-1 mr-2">
-        <nuxt-link class="btn block" to="/">Home</nuxt-link>
-      </li>
-      <li class="flex-1 mr-2">
-        <nuxt-link class="btn block" to="/blog">Blog</nuxt-link>
-      </li>
-      <li class="flex-1">
-        <nuxt-link class="btn block" to="/projects">Projects</nuxt-link>
-      </li>
-    </ul>
-  </nav>
+    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <svg
+          class="fill-current h-8 w-8 mr-2"
+          width="54"
+          height="54"
+          viewBox="0 0 54 54"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"
+          />
+        </svg>
+        <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+      </div>
+      <div class="block lg:hidden">
+        <button
+          id="hamburgerbtn"
+          class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+        >
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <ul id="mobileMenu" class="text-sm lg:flex-grow">
+          <li>
+            <nuxt-link to="/" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Inicio
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/blog" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Blog
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/projects" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+              Projects
+            </nuxt-link>
+          </li>
+        </ul>
+        <div>
+          <a
+            href="#"
+            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            >Download</a
+          >
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -35,5 +77,9 @@ export default {
       @apply text-white;
     }
   }
+}
+
+.active {
+  display: block;
 }
 </style>
