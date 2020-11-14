ef68b280ab62a948356b15a58bd3d11fdf5f2ae7
xacobe <xacobe@cambrico.net>
2020-11-14 17:50:24 +0100

    build blog layout

--- components/general/Header.vue
+++ components/general/Header.vue
@@ -1,89 +1,120 @@
 <template>
-  <header class="bg-green text-white w-full top-0 left-0 p-8">
-    <div class="sm:mx-auto flex flex-wrap justify-between">
-      <div class="logo w-16 sm:w-24">
-        <a href="/">
-          <img
-            class="w-full"
-            src="../../assets/img/logo.svg"
-            alt="Asociación do monte galego"
-            width="100px"
-            height="132.15"
-          />
-        </a>
-      </div>
-      <div class="flex flex-wrap">
-        <!-- Desktop nav list -->
-        <nav>
-          <ul id="mobileMenu" class="text-base sm:text-lg flex uppercase">
-            <li>
-              <nuxt-link to="/" class="mr-4"> Inicio </nuxt-link>
-            </li>
-            <li>
-              <nuxt-link to="/blog" class="mr-4"> Noticias </nuxt-link>
-            </li>
-            <li>
-              <nuxt-link to="/projects" class=""> Proyectos </nuxt-link>
-            </li>
-          </ul>
-        </nav>
-        <!-- Toggle menu mobile icon -->
-        <!-- <div class="ml-auto sm:hidden" @click="menu = !menu">
-          <font-awesome-icon icon="bars" style="font-size: 2rem" />
-        </div> -->
-        <!-- Mobile nav list -->
-        <!-- <nav v-show="menu" class="w-full">
-          <ul class="flex flex-col text-center">
-            <li v-for="(item, index) in items" :key="index">
-              <a :href="item.href" class="hover:bg-green-500 p-6 block">
-                {{ item.title }}
-              </a>
-            </li>
-          </ul>
-        </nav> -->
-      </div>
+  <header class="bg-green text-white w-full top-0 left-0">
+    <nav class="main-menu bg-gray-100 hidden sm:block">
+      <ul class="flex text-base sm:text-lg px-8 py-4 justify-end uppercase">
+        <li>
+          <nuxt-link to="/" class="mr-4"> Inicio </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/blog" class="mr-4"> Noticias </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/projects" class=""> Proyectos </nuxt-link>
+        </li>
+      </ul>
+    </nav>
+
+    <!-- Toggle menu mobile icon -->
+    <div class="sm:hidden absolute px-8 py-4 right-0" @click="menu = !menu">
+      <font-awesome-icon icon="bars" style="font-size: 2rem" />
     </div>
+
+    <!-- Logo -->
+    <div class="logo w-16 sm:w-24 mx-8 my-4">
+      <a href="/">
+        <img
+          class="w-full"
+          src="../../assets/img/logo.svg"
+          alt="Asociación do monte galego"
+          width="100px"
+          height="132.15"
+        />
+      </a>
+    </div>
+
+    <!-- Mobile nav list -->
+    <nav v-show="menu" class="mobile-menu w-full bg-white text-center text-lg uppercase">
+      <ul class="flex flex-col">
+        <li v-for="(item, index) in items" :key="index">
+          <a :href="item.href" class="p-6 block">
+            {{ item.title }}
+          </a>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
 <script>
 export default {
   name: 'Header',
-  // data() {
-  //   return {
-  //     menu: false,
-  //     items: [
-  //       {
-  //         title: 'Home',
-  //         href: '/',
-  //       },
-  //       {
-  //         title: 'Blog',
-  //         href: '/blog',
-  //       },
-  //       {
-  //         title: 'Projects',
-  //         href: '/projects',
-  //       },
-  //     ],
-  //   }
-  // },
+  data() {
+    return {
+      menu: false,
+      items: [
+        {
+          title: 'Inicio',
+          href: '/',
+        },
+        {
+          title: 'Noticias',
+          href: '/blog',
+        },
+        {
+          title: 'Proyectos',
+          href: '/projects',
+        },
+      ],
+    }
+  },
 }
 </script>
 
 <style lang="postcss" scoped>
 .nuxt-link-exact-active {
   @apply text-green-100 border-b-8 border-primary-900;
-  &:hover {
-    @apply text-white;
+}
+.mobile-menu li a {
+  position: relative;
+  border-bottom: solid 1px darkgrey;
+  overflow: hidden;
+
+  &:hover,
+  &:focus {
+    @apply bg-blue-100 underline;
+  }
+
+  &:focus-within {
+    background: #c9ecff;
+    @apply flex justify-center;
+    &::before {
+      content: '';
+      display: block;
+      width: 1px;
+      height: 1px;
+      background: rgb(124, 170, 255);
+      opacity: 0;
+      border-radius: 100%;
+      position: absolute;
+      animation: circlepop 1s forwards;
+    }
+  }
+}
+@keyframes circlepop {
+  from {
+    opacity: 1;
+    transform: scale(0);
+    transform-origin: center;
+  }
+  to {
+    opacity: 0;
+    transform: scale(500);
+    transform-origin: center;
   }
 }
 .light-mode {
   & .nuxt-link-exact-active {
     @apply text-primary-900;
-    &:hover {
-      @apply text-white;
-    }
   }
 }
 </style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,9 +1,9 @@
 <template>
   <main v-if="blogPosts" class="main">
-    <h2 class="title text-left pt-5">Blog</h2>
-    <ul v-for="(blogPost, index) in blogPosts" :key="index" class="articles">
-      <nuxt-link :to="`blog/${blogPost.slug}`" class="article article--clickable">
-        <div class="flex justify-between align-baseline">
+    <h2 class="title text-left">Blog</h2>
+    <ol class="grid gap-4 grid-cols-1 sm:gap-8 sm:grid-cols-2 lg:grid-cols-3">
+      <li v-for="(blogPost, index) in blogPosts" :key="index" class="articles">
+        <nuxt-link :to="`blog/${blogPost.slug}`" class="article article--clickable">
           <h3 class="article-title">{{ blogPost.title }}</h3>
           <h6
             v-if="blogPost.date"
@@ -11,12 +11,12 @@
           >
             {{ formatDate(blogPost.date) }}
           </h6>
-        </div>
-        <div class="mt-4 mb-2">
-          <p class="inline">{{ blogPost.description }}</p>
-        </div>
-      </nuxt-link>
-    </ul>
+          <div class="mt-4 mb-2 font-light">
+            <p class="inline">{{ blogPost.description }}</p>
+          </div>
+        </nuxt-link>
+      </li>
+    </ol>
   </main>
 </template>
 <script>
