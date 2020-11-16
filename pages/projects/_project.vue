c6dc2ebfcafe3316d698111139bca19115049885
xacobe <xacobe@cambrico.net>
2020-11-16 04:49:00 +0100

    minor fixes

--- pages/projects/_project.vue
+++ pages/projects/_project.vue
@@ -1,9 +1,11 @@
 <template>
   <article v-if="projectPost" class="main article">
-    <h1 class="article-title">{{ projectPost.title }}</h1>
-    <p class="mt-4">{{ projectPost.description }}</p>
     <img class="cover-image" :src="projectPost.cover" />
-    <div class="block mt-8 mb-4" v-html="$md.render(projectPost.body)" />
+    <div class="px-6 py-4 bg-white">
+      <h2 class="article-title text-3xl">{{ projectPost.title }}</h2>
+      <p class="mt-4">{{ projectPost.description }}</p>
+      <div class="block mt-8 mb-4 b-white" v-html="$md.render(projectPost.body)" />
+    </div>
     <div v-if="projectPost.gallery">
       <img v-for="image in projectPost.gallery" class="image" :key="image.id" :src="image" />
     </div>
@@ -18,6 +20,5 @@ export default {
         projectPost: await require(`~/assets/content/projects/${params.project}.json`),
       }
   },
-  f,
 }
 </script>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,35 +1,34 @@
 <template>
-  <main v-if="projectPosts" class="main">
-    <h1 class="title text-left text-gray-800">Projects</h1>
-    <ol>
-      <li
-        v-for="(projectPost, index) in projectPosts"
-        :key="index"
-        class="articles flex mx-auto bg-white overflow-hidden shadow-md"
-      >
-        <nuxt-link class="article article--clickable flex flex-row" :to="`projects/${projectPost.slug}`">
-          <div class="w-1/2">
-            <img
-              class="w-full h-full object-cover flex-1"
-              src="https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-              alt="Article"
-            />
-          </div>
-
-          <div class="p-6">
-            <div>
-              <span class="text-blue-600 text-xs font-medium uppercase">{{ projectPost.project_type }}</span>
-              <a href="#" class="block text-gray-800 font-semibold text-2xl mt-2 hover:text-gray-600 hover:underline">{{
-                projectPost.title
-              }}</a>
-              <p class="text-sm text-gray-600 mt-2">
-                {{ projectPost.description }}
-              </p>
+  <main class="container">
+    <section v-if="projectPosts" class="main">
+      <h1 class="title text-left pb-2 text-gray-800">Projects</h1>
+      <ol>
+        <li
+          v-for="(projectPost, index) in projectPosts"
+          :key="index"
+          class="articles flex mx-auto bg-white overflow-hidden shadow-md mb-6"
+        >
+          <nuxt-link class="article article--clickable" :to="`projects/${projectPost.slug}`">
+            <div class="flex flex-row">
+              <div class="w-1/2 md:w-1/2">
+                <img class="w-full h-full object-cover flex-1" :src="projectPost.cover" alt="Article" />
+              </div>
+              <div class="w-2/3 md:w-1/2 pl-6">
+                <span class="text-blue-600 text-xs font-medium uppercase">{{ projectPost.project_type }}</span>
+                <a
+                  href="#"
+                  class="block text-gray-800 font-semibold text-2xl mt-2 hover:text-gray-600 hover:underline"
+                  >{{ projectPost.title }}</a
+                >
+                <p class="text-sm text-gray-600 mt-2">
+                  {{ projectPost.description }}
+                </p>
+              </div>
             </div>
-          </div>
-        </nuxt-link>
-      </li>
-    </ol>
+          </nuxt-link>
+        </li>
+      </ol>
+    </section>
   </main>
 </template>
 <script>
